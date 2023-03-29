ac7630e013cb3e2901d603c7606eb485f4a3f1c3
Felipe Mendes <felipetec097@outlook.com>
2023-03-29 07:57:45 -0300

    primeiros passos em Ruby

---
+++ atividade.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá me chamo #{nome} #{sobrenome} tenho #{idade} anos."
